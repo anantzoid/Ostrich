064ec1b9267851a3b824c248d026e5ebd7978830
anantzoid <anant718@gmail.com>
2015-10-15 18:31:14 +0000

    change in order logic to support incremental inventory

--- app/models/order.py
+++ app/models/order.py
@@ -4,12 +4,12 @@ import datetime
 #from app.models import User, Item
 
 class Order():
-    def __init__(self, item_id, user_id, address_id, order_return=None):
+    def __init__(self, item_ids, user_id, address_id, order_return=None):
         #self.user = User(user_id)
         #self.item = Item(item_id)
 
         self.user = user_id
-        self.item = item_id
+        self.items = item_ids
         self.address_id = address_id
         
         self.order_placed = self.getCurrentTimestamp()
@@ -19,67 +19,79 @@ class Order():
         self.connect = mysql.connect()                    
 
     def placeOrder(self):
-        '''
-        Flow:
-            Check if user is not blocked
-            Check item availability in inventory
-            Check if user has currently borrowed the same item
-            Place order
-            Insert inventory_id and order_id
-            Update stock availability
-        '''
-        
+       
         #check user validity
 
-        inventory_id = self.checkItemAvailability() 
-        if not inventory_id:
-            return {'message': 'Item out of stock'}
-
-
+       inventory_ids = self.getInventoryIds() 
+        
+        '''
+        #NOTE Skipping this too for now
         #check if user is ordering same item in same period
-        check_record_cursor = self.connect.cursor()
-        check_record_cursor.execute("SELECT order_id FROM orders WHERE user_id = %d AND item_id = %d AND UNIX_TIMESTAMP(order_return) <= UNIX_TIMESTAMP('%s')" %(self.user, self.item, self.order_placed))
-        record_count = check_record_cursor.fetchone()
-        check_record_cursor.close()
-
-        if record_count:
+        if not self.checkOrderValidity():
             return {'message': 'Can only order after returning'}
-
+        '''
+                
         insert_data_cursor = self.connect.cursor()
-        insert_data_cursor.execute("INSERT INTO orders (item_id, user_id, address_id, order_placed, order_return) VALUES(%d, %d, %d, '%s', '%s')" %(self.item, self.user, self.address_id, self.order_placed, self.order_return) )
+        insert_data_cursor.execute("INSERT INTO orders (user_id, address_id, \ 
+                order_placed, order_return) VALUES(%d, %d, '%s', '%s')" % \
+                (self.user, self.address_id, self.order_placed, self.order_return) )
         self.connect.commit()
         order_id = insert_data_cursor.lastrowid
         insert_data_cursor.close()
 
-        order_history_cursor = self.connect.cursor()
-        order_history_cursor.execute("INSERT INTO order_history (inventory_id, order_id) VALUES (%d, %d)" %(inventory_id, order_id))
-        order_history_cursor.close()
+        for inventory_id in inventory_ids:
+            order_history_cursor = self.connect.cursor()
+            order_history_cursor.execute("INSERT INTO order_history (inventory_id, order_id) VALUES (%d, %d)" %(inventory_id, order_id))
+            self.connect.commit()
+            order_history_cursor.close()
 
-        update_stock_cursor = self.connect.cursor()
-        update_stock_cursor.execute("UPDATE inventory SET in_stock = 0 WHERE inventory_id = %d" % (inventory_id))
-        self.connect.commit()
-        update_stock_cursor.close()
+            update_stock_cursor = self.connect.cursor()
+            update_stock_cursor.execute("UPDATE inventory SET in_stock = 0 WHERE inventory_id = %d" % (inventory_id))
+            self.connect.commit()
+            update_stock_cursor.close()
 
         return {'order_id': order_id}
     
 
-    def checkItemAvailability(self):
-        item_check_cursor = self.connect.cursor()
-        item_check_cursor.execute("SELECT inventory_id, lender_id FROM inventory WHERE item_id = %d AND in_stock = 1 ORDER BY date_added" % (self.item))
-        items = item_check_cursor.fetchall()
-        item_check_cursor.close()
-        
-        if not items:
-            return None
-        
-        # check if a lender's item is present,
-        # else return the inventory item
+    def checkOrderValidity(self):
+        check_record_cursor = self.connect.cursor()
+        check_record_cursor.execute("SELECT order_id FROM orders WHERE user_id = %d AND item_id = %d AND UNIX_TIMESTAMP(order_return) <= UNIX_TIMESTAMP('%s')" %(self.user, self.item, self.order_placed))
+        record_count = check_record_cursor.fetchone()
+        check_record_cursor.close()
+        return 0 if record_count else 1
 
-        for item in items:
-            if item[1]:
-                return int(item[0]) 
+    def getInventoryIds(self):
+        
+        inventory_ids = []
+        for item_id in self.items:
+            item_check_cursor = self.connect.cursor()
+            item_check_cursor.execute("SELECT inventory_id, lender_id FROM inventory \
+                    WHERE item_id = %d AND in_stock = 1 ORDER BY date_added" % (item_id))
+            inv_items = item_check_cursor.fetchall()
+            item_check_cursor.close()
 
-        return int(items[0][0])
+            if inv_items: 
+                # check if a lender's item is present,
+                # else return the inventory item
+                inv_item_selected = inv_items[0][0]
+                for item in inv_items:
+                    if item[1]:
+                        inv_item_selected = item[0]
+                        break
+
+                inventory_ids.append(inv_item_selected)
+            else:
+                #TODO change this logic once we stop incremental inventory
+                insert_inv_item = self.connect.cursor()
+                insert_inv_item.execute("INSERT INTO inventory (item_id) VALUES ('%s')" %(item_id))
+                self.connect.commit()
+                new_inv_id = insert_inv_item.lastrowid
+                insert_inv_item.close()
+
+                inventory_ids.append(new_inv_id)
+
+
+        return inventory_ids
 
     
     def getCurrentTimestamp(self):
--- app/views/order.py
+++ app/views/order.py
@@ -6,15 +6,15 @@ from flask import request, jsonify
 def OrderItem():
     
     #TODO make return pattern consistent
-    item_id = int(request.form['item_id']) if 'item_id' in request.form else ''
+    item_ids = request.form['item_id']) if 'item_id' in request.form else ''
     user_id = int(request.form['user_id']) if 'user_id' in request.form else ''
     address_id = int(request.form['address_id']) if 'address_id' in request.form else ''
    
-    if not(item_id or user_id or address_id):
-        return jsonify({'status':'False'})
+    if not(item_ids or user_id or address_id):
+        return jsonify({'status':'False', 'message':'Parameters missing'})
 
-    #TODO make these objects
-    order = Order(item_id, user_id, address_id)
+    item_ids = [int(item_id) for item_id in item_ids.split(',')]
+    order = Order(item_ids, user_id, address_id)
     order_placed = order.placeOrder()
 
     if 'order_id' in order_placed:
