39877e3e7e1d948c6173b18460a0a7fa8b32f391
anantzoid <anant718@gmail.com>
2016-01-04 14:22:42 +0530

    utc timezone

--- scheduler.py
+++ scheduler.py
@@ -6,12 +6,13 @@ from celery.utils.log import get_task_logger
 
 from app.scripts.create_celery_app import createCeleryApp
 celery = createCeleryApp(webapp)
+# NOTE times in crontab are in UTC
 
 '''
     Run everyday at 10am to prompt users to extend the return date if the latter
     is 3 days later 
 '''
-@periodic_task(run_every=(crontab(hour="10", minute="0")))
+@periodic_task(run_every=(crontab(hour="04", minute="30")))
 def returnDateExtensionReminder():
     from app.scripts.return_date_extension_reminder import returnDateExtensionReminder
     returnDateExtensionReminder()
@@ -19,7 +20,7 @@ def returnDateExtensionReminder():
 '''
     Run everyday at 6pm to prompt users to give a pickup time for the order
 '''
-@periodic_task(run_every=(crontab(hour="18", minute="0")))
+@periodic_task(run_every=(crontab(hour="12", minute="30")))
 def pickupTimeSlot():
     from app.scripts.pickup_timeslot import pickupTimeslot
     pickupTimeslot()
@@ -27,7 +28,7 @@ def pickupTimeSlot():
 '''
     Run everyday at 7am to show all pickups/deliveries for the day
 '''
-@periodic_task(run_every=(crontab(hour="7", minute="0")))
+@periodic_task(run_every=(crontab(hour="01", minute="30")))
 def pickupSchedule():
     from app.scripts.pickup_schedule import pickupSchedule
     pickupSchedule()
