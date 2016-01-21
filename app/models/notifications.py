3c725b212fa13064f14f5eefed6473bcc5734b63
anantzoid <anant718@gmail.com>
2016-01-21 19:04:05 +0530

    search fail notification

--- app/models/notifications.py
+++ app/models/notifications.py
@@ -1,5 +1,6 @@
 from gcm import GCM
 from app import webapp
+from app import mysql 
 
 class Notifications():
     def __init__(self, gcm_id):
@@ -12,3 +13,21 @@ class Notifications():
             notification_status = self.gcm.json_request(registration_ids=self.gcm_id, data=data)
             return notification_status
 
+    def itemAvailability(self, item_id, flow, fail_id):
+        from app.models import Item
+        item = Item(item_id)
+        data  = {
+                "notification_id": 5,
+                "entity_id": item.item_id,
+                "title": "%s is now available" % (item.item_name),
+                "message": "The book you were searching for is now available",
+                "expanded_text": "The book you were searching for is now available",
+                "item_name": item.item_name,
+                "search_intention": flow
+                }
+        response = self.sendNotification(data)
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("""UPDATE search_fails SET gcm_token = %s WHERE id = %s"""
+                ,(response, fail_id))
+        conn.commit()
--- manage.py
+++ manage.py
@@ -13,5 +13,15 @@ def indexer():
     from app.scripts import Indexer
     Indexer().indexItems(query_condition=' AND (i.item_id = 1 OR i.item_id=79)')
 
+@manager.command
+def searchfail():
+    from app.models import Notifications, User, Item
+    # user_id, item_id, flow
+    data = [[76, 1, 'borrow', 1]]
+
+    for row in data:
+        user = User(row[0])
+        Notifications(user.gcm_id).itemAvailability(row[1], row[2], row[3])
+
 if __name__ == "__main__":
     manager.run()
