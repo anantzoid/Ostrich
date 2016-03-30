c7e47674eafe84b072733edff975e19a73ae7dfd
Anant Gupta <anant718@gmail.com>
2016-03-30 16:09:11 +0530

    collections metadata

--- app/models/collection.py
+++ app/models/collection.py
@@ -12,8 +12,14 @@ class Collection(Prototype):
             where ci.collection_id = c.collection_id and ci.active = 1) as item_ids,
             (select group_concat(concat(cm.meta_key,":",cm.meta_value) separator '&') from collections_metadata cm 
             where cm.collection_id = c.collection_id) as metadata
-            FROM c.collections WHERE c.collection_id = %s""", (collection_id,))
+            FROM collections c WHERE c.collection_id = %s""", (collection_id,))
         self.data = Utils.fetchOneAssoc(cursor)
+
+        collections_metadata_raw = self.data['metadata']
+        self.data['metadata'] = {}
+        for props in collections_metadata_raw.split('&'):
+            props_formatted = props.split(':')
+            self.data['metadata'][props_formatted[0]] = props_formatted[1]
         if not self.data:
             self.data = {}
         
