f8d8665f3c9dd299fbaa63bfb80af90b55cb04ac
Anant Gupta <anant718@gmail.com>
2016-03-30 14:35:18 +0530

    log price of order item

--- app/models/order.py
+++ app/models/order.py
@@ -64,8 +64,9 @@ class Order():
             order_info['order_id'] = children[-1]['order_id']
 
         all_orders = parents + children
-        for order in all_orders:
-            charge += order['charge']
+        for i, order in enumerate(all_orders):
+            if i < len(all_orders) - 1:
+                charge += order['charge']
             order_info['all_charges'].append({
                     'charge': Order.getCharge(order['charge']),
                     'payment_mode': order['payment_mode']
@@ -137,7 +138,12 @@ class Order():
         order_data['delivery_date'] = Utils.getParam(order_data, 'delivery_date', default = order_data['order_placed'])
         order_data['order_return'] = Utils.getParam(order_data, 'order_return', 
                 default = Utils.getDefaultReturnTimestamp(order_data['delivery_date'], webapp.config['DEFAULT_RETURN_DAYS']))
-        order_data['order_amount'] = int(webapp.config['DEFAULT_RETURN_DAYS'] * webapp.config['NEW_READING_RATE'])  
+
+        default_order_amount = int(webapp.config['DEFAULT_RETURN_DAYS'] * webapp.config['NEW_READING_RATE'])
+        item = Item(order_data['item_id'])
+        if item.price >= 399:
+            default_order_amount = 60
+        order_data['order_amount'] =  Utils.getParam(order_data, 'order_amount', 'int', default_order_amount) 
 
         #check order validity
         # TODO check if item exists
@@ -158,8 +164,9 @@ class Order():
                 delivery_date,
                 delivery_slot, 
                 pickup_slot, 
-                payment_mode) 
-                VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"""  
+                payment_mode,
+                charge) 
+                VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"""  
                 ,(order_data['user_id'], 
                     order_data['address']['address_id'], 
                     order_data['order_placed'], 
@@ -167,7 +174,8 @@ class Order():
                     order_data['delivery_date'], 
                     order_data['delivery_slot'], 
                     order_data['delivery_slot'], 
-                    order_data['payment_mode']))
+                    order_data['payment_mode'],
+                    order_data['order_amount']))
         connect.commit()
         order_id = insert_data_cursor.lastrowid
         insert_data_cursor.close()
