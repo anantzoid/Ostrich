32aca58680f0ee7a92ee7fa3d23d093400814a2c
Anant Gupta <anant718@gmail.com>
2016-04-04 13:27:55 +0530

    fix in upsell mail

--- app/scripts/upsell_email.py
+++ app/scripts/upsell_email.py
@@ -12,8 +12,9 @@ def upsellEmail(order_id):
     order_info = Order(order_id).getOrderInfo()
     client = MongoClient(webapp.config['MONGO_DB'])
     db = client.ostrich
-    
-    first_item_id = order_info['items'][0]['item_id']
+   
+    first_item = order_info['items'][0]
+    first_item_id = first_item['item_id']
     related_items_cursor = db.related_item_ids.find({'_id': first_item_id})
     related_item_ids = [_ for _ in related_items_cursor]
 
@@ -38,7 +39,7 @@ def upsellEmail(order_id):
     data = {
             "user": User(order_info['user_id']),
             "items": items[:4], 
-            "book_name": order_info["item"]["item_name"],
+            "book_name": first_item["item_name"],
             "order_id": order_info["order_id"],
             "curated_items": curated_items,
             "quote": quote, 
