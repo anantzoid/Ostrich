e4a914eccbb548caf8be7b96f6fe76d1debe08f0
Anant Gupta <anant718@gmail.com>
2016-04-20 17:16:09 +0530

    doctype fix

--- app/views/v1/website.py
+++ app/views/v1/website.py
@@ -9,10 +9,16 @@ components_path = os.path.join(os.getcwd(), 'app', 'static', 'js', 'components')
 def path(js_file):
     return os.path.join(components_path, js_file)
 
+def getTitle(path):
+    if (path == 'home'):
+        return 'Ostrich: Home'
+    return 'Ostrich'
+
 @webapp.route('/')
 def homepage():
     panel_data = [Collection(4).getExpandedObj(), Collection(5).getExpandedObj()]
     
     rendered = render_component(path('home.jsx'), props={'panel_data': panel_data})
-    return render_template(base_view, rendered=rendered)
+    title = getTitle('home')
+    return render_template(base_view, rendered=rendered, title=title)
 
