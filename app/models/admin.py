e099c832a1c2d969831005b29b1af2071982fcbe
Anant Gupta <anant718@gmail.com>
2016-04-30 15:55:23 +0530

    wishlist in dashboard

--- app/models/admin.py
+++ app/models/admin.py
@@ -98,6 +98,7 @@ class Admin():
             order_info = order.getOrderInfo()
             user = User(order_info['user_id'])
             order_info['user'] = user.getObj()
+            order_info['user']['wishlist'] = User.getWishlist(order_info['user_id'])
             order_info['address'] = user.getAddressInfo(order_info['address_id']) 
             order_info['delivery_slot'] = [ts for ts in all_time_slots if ts['slot_id'] == order_info['delivery_slot']][0]
             order_info['pickup_slot'] = [ts for ts in all_time_slots if ts['slot_id'] == order_info['pickup_slot']][0]
