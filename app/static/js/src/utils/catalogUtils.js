73b0c725c9c43692a69c6fd4174f4fde2cc16670
Anant Gupta <anant718@gmail.com>
2016-06-03 01:37:00 +0530

    multi page for category search

--- app/static/js/src/utils/catalogUtils.js
+++ app/static/js/src/utils/catalogUtils.js
@@ -2,17 +2,26 @@ import loaderPlaceholder from './loader';
 
 let CatalogUtils = {
     loadMore(page) {
- loaderPlaceholder(true,  '.load-more'); 
         return new Promise((resolve, reject) => {
             let className = '.load-more';
-            let params = window.location.search;
-            params = params.replace(/page=\d*/,'');
+            let params = {};
+            window.location.search.substr(1).split('&').forEach(function(param) {
+                let _ = param.split('=');
+                params[_[0]] = _[1];
+            });
+            //NOTE only handled for category
+            if (!params.hasOwnProperty('q')) {
+                let page_type = window.location.pathname.split("/");
+                params['type'] = page_type[2];
+                params['q'] = page_type[3].replace(/\w\S*/g, ((txt) => {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                }));
+            }
+            params.page = page;
+            params.ref = 'web';
             $.ajax({
-                url: '/search' + params,
-                data: {
-                    page: page,
-                    ref: 'web'
-                },
+                url: '/search',
+                data: params,
                 beforeSend: () => { loaderPlaceholder(true, className); },
                 success: ((response) => {
                     try {
