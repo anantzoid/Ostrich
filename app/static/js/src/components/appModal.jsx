67b084c0e37b739b9538b6249b33a26c2886b632
Anant Gupta <anant718@gmail.com>
2016-06-08 14:29:15 +0530

    homepage & app-modal styling

--- app/static/js/src/components/appModal.jsx
+++ app/static/js/src/components/appModal.jsx
@@ -7,21 +7,14 @@ const AppModal = React.createClass({
         return (
             <Modal show={this.props.show} onHide={this.props.hide} bsSize="sm">
                 <Modal.Header closeButton>
-                    <Modal.Title>Order Placed Successfully</Modal.Title>
+                    <Modal.Title>Download our app from Android Store</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                   
-                    <div>Download app from Android Store to</div>
                     <ul>
-                        <li>
-                            <span className="glyphicon glyphicon-star gold" aria-hidden="true"></span> Order any book in a few taps
-                        </li>
-                        <li>
-                            <span className="glyphicon glyphicon-star gold" aria-hidden="true"></span> Keep track of your orders and extend them
-                        </li>
-                        <li>
-                            <span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>  Share with your friends
-                        </li>
+                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Rent any book in a few taps</li>
+                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Keep track and extend your orders</li>
+                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Order Collection Sets</li>
+                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Share with your friends</li>
                     </ul> 
                     <div className="install-container clearfix">
                         <a target="_blank" href='https://play.google.com/store/apps/details?id=in.hasslefree.ostrichbooks&hl=en&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img id="play-store" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
--- app/static/js/src/components/home.jsx
+++ app/static/js/src/components/home.jsx
@@ -119,12 +119,20 @@ const Homepage = React.createClass({
                         <div className="row">
                             <div className="col-lg-6 text-center">
                                 <img className="app-img" src="/static/img/tmp/showcase.png" />
+                            </div>
+                            <div className="col-lg-2"></div>
+                            <div className="col-lg-6">
+                                <div className="app-list-container">
+                                    <ul className="app-list">
+                                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Rent any book in a few taps</li>
+                                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Keep track and extend your orders</li>
+                                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Order Collection Sets</li>
+                                        <li><span className="glyphicon glyphicon-star gold" aria-hidden="true"></span>Share with your friends</li>
+                                    </ul>
+                                </div>
                                 <div className="app-link"><a target="_blank" href='https://play.google.com/store/apps/details?id=in.hasslefree.ostrichbooks&hl=en&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img id="play-store" alt='Get it on Google Play' width="196" src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a></div>
                             </div>
-                            <div className="col-lg-6 text-center">
-                                <img className="app-img" src="/static/img/tmp/slack-demo.png" />
-                                <div className="app-link"><a href="https://slack.com/oauth/authorize?scope=users:read,incoming-webhook,commands&client_id=4256151918.31600537060&redirect_uri=https://ostrich-slack.herokuapp.com/oauth"><img alt="Add to Slack" height="55" width="196" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a></div>
-                            </div>
+                            
                         </div>
                     </div>
                 </section>
@@ -137,4 +145,9 @@ const Homepage = React.createClass({
 // not compatible with React.createFactory
 //export default homepage;
 module.exports = Homepage;
-
+/*
+<div className="col-lg-6 text-center">
+                                <img className="app-img" src="/static/img/tmp/slack-demo.png" />
+                                <div className="app-link"><a href="https://slack.com/oauth/authorize?scope=users:read,incoming-webhook,commands&client_id=4256151918.31600537060&redirect_uri=https://ostrich-slack.herokuapp.com/oauth"><img alt="Add to Slack" height="55" width="196" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a></div>
+                            </div> 
+ */
